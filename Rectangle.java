8f7e050d4646c060b5e4cc83d5bf32528f2c1188
ShushantaDhungana <99549214+ShushantaDhungana@users.noreply.github.com>
2022-02-15 19:55:18 +0545

    Add files via upload

---
+++ Rectangle.java
@@ -0,0 +1,13 @@
+    import java.util.Scanner;
+public class Rectangle{
+    public static void main (String args[]){
+        int width = 10;
+        int height = 15;
+        int area = width*height;
+        System.out.print("area of rectangle = "+area);
+
+
+
+    }
+
+}
